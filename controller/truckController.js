a89c25bc05be92565fc7838569f560e28ba71bf1
Jose Joaquin <jomerinog@gmail.com>
2015-01-21 17:32:47 -0800

    added rediscloud url for heroku

--- controller/truckController.js
+++ controller/truckController.js
@@ -1,7 +1,12 @@
 var truckRepository = require('../model/truckRepository'),
   redis = require('redis'),
-  client = redis.createClient(),
-  proximity = require('geo-proximity')
+  url = require('url'),
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+
+client.auth(redisURL.auth.split(":")[1]);
+
+var proximity = require('geo-proximity')
     .initialize(client,"truck:locations");
 
 
