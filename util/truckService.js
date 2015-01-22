326ea3eca735af38aef0ecbc1e34da8ae1f2e8ec
Jose Joaquin <jomerinog@gmail.com>
2015-01-22 05:05:06 -0800

    remove unused legacy methods

--- util/truckService.js
+++ util/truckService.js
@@ -2,40 +2,9 @@ var q = require('q');
 var request = require('request');
 
 /**
- * Provides an abstraction layer to query the DataSF foot trucks api
- */
-var TruckService = function(){
-
-};
-/**
- * Finds trucks in the enclosing rectangle by querying the API
- */
-TruckService .prototype.findInArea = function(lat1,lng1,lat2,lng2){
-  var d = q.defer();
-
-  // Query construction
-  var qs = 'https://data.sfgov.org/resource/rqzj-sfat.json?$where='+
-    'latitude > ' + +lat1 +
-    ' AND latitude < ' + +lat2 +
-    ' AND longitude > ' + +lng1 +
-    ' AND longitude < ' + +lng2;
-
-  request(qs,
-    function (err, res) {
-      if(err){
-        console.error(err);
-        return d.reject(err);
-      }
-
-      d.resolve(JSON.parse(res.body));
-    }
-  );
-  return d.promise;
-};
-/**
  * Fetches all trucks. Use sparingly.
  */
-TruckService .prototype.findAll = function(){
+exports.findAll = function(){
   var d = q.defer();
 
   // Query construction
@@ -51,5 +20,4 @@ TruckService .prototype.findAll = function(){
   );
   return d.promise;
 };
-module.exports = new TruckService();
 
